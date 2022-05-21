769d95586e1434762a37c7a299abc80412a84e58
David Villada <david@21unicorns.com>
2022-05-21 15:35:56 -0500

    fix: faker data url

--- src/utils/faker.ts
+++ src/utils/faker.ts
@@ -1,10 +1,25 @@
 import { faker } from '@faker-js/faker';
+
 export function getUserList(usersLength = 10) {
-  const users = Array.apply(null, { length: usersLength }).map(() => ({
+  const users = Array.apply(null, { length: usersLength }).map((_, index) => ({
+    id: index,
     name: `${faker.name.firstName()} ${faker.name.lastName()}`,
     photo: {
-      url: faker.image.avatar(),
+      url: imageArray[faker.random.numeric(1)],
     },
   }));
   return users;
 }
+
+const imageArray = [
+  'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/291.jpg',
+  'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/936.jpg',
+  'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/214.jpg',
+  'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/325.jpg',
+  'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/367.jpg',
+  'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1139.jpg',
+  'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/222.jpg',
+  'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1222.jpg',
+  'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/246.jpg',
+  'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/459.jpg',
+];
